c1b523a47a9c710b794e5435ae4beabca5ac5520
YousefKullab <the1yousef98@gmail.com>
2022-12-29 19:50:46 +0200

    upadte urls file

--- InfoS/settings.py
+++ InfoS/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-59ogqyggd+-p$cz$r&yp#0cl@g*sl9&*3bzv6n75_qn_em^%n*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
